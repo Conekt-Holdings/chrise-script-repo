5d5272790bb5764dcb51ec0a034ba0474fed8101
chris-sorandom <161941128+chris-sorandom@users.noreply.github.com>
2025-07-30 09:33:32 +0200

    Create Set DNS Resolvers for All Interfaces to DHCP.ps1

---
+++ General/Network/Set DNS Resolvers for All Interfaces to DHCP.ps1
@@ -0,0 +1,5 @@
+# Set DNS Resolvers for All Interfaces to DHCP
+ForEach ($name in Get-NetAdapter | select -expand name) {
+    $name
+    netsh interface ipv4 set dnsservers name=$name source=dhcp
+    }
