488e552b4f7b0277280b04a2e8bb346ed8856070
Christopher Evans <chris@sorandom.co.za>
2025-11-12 08:28:58 +0200

    Update Unifi controller service installation guide

--- General/Unifi/Install Unifi As Service.bat
+++ General/Unifi/Install Unifi As Service.bat
@@ -1,8 +1,15 @@
-# Uninstall Service
+rem # Uninstall Service
 cd "%UserProfile%\Ubiquiti UniFi\"
 java -jar lib\ace.jar uninstallsvc
-# Re-Install and Start Service
+rem # Re-Install and Start Service
 java -jar lib\ace.jar installsvc
 java -jar lib\ace.jar startsvc
 
-# as per https://help.ui.com/hc/en-us/articles/205144550-Self-Hosting-a-UniFi-Network-Server-as-a-Windows-Service-Advanced
+rem # as per https://help.ui.com/hc/en-us/articles/205144550-Self-Hosting-a-UniFi-Network-Server-as-a-Windows-Service-Advanced
+
+rem # If you do received an error stating that a reg path could not be found, or that some java dll files could not be found, you may need to install the service as per below:
+rem # This is relevant if using Adoptium 17
+rem # cd "%UserProfile%\Ubiquiti UniFi\"
+rem # "C:\Program Files\Eclipse Adoptium\jre-17.0.17.10-hotspot\bin\Java.exe" -jar lib\ace.jar uninstallsvc
+rem # "C:\Program Files\Eclipse Adoptium\jre-17.0.17.10-hotspot\bin\Java.exe" -jar lib\ace.jar installsvc
+rem # "C:\Program Files\Eclipse Adoptium\jre-17.0.17.10-hotspot\bin\Java.exe" -jar lib\ace.jar startsvc
